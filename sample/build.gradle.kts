ed217687e93b845f3fa26a441f5426a61f315b4c
jisungbin <ji@sungb.in>
2023-04-18 03:42:05 +0900

    Update sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -10,23 +10,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.8.20"
     `kotlin-dsl`
-    // id("land.sungbin.dependency.graph.plugin") version "1.0.0"
+    id("land.sungbin.dependency.graph.plugin") version "1.1.0"
 }
 
-// dependencyGraphConfigs {
-//     projectName = "dependency-graph-sample"
-//
-//     dependencyBuilder { project ->
-//         with(project.plugins) {
-//             when {
-//                 hasPlugin("project.three") -> DependencyInfo("#81d4fa", isBoxShape = true)
-//                 hasPlugin("project.two") -> DependencyInfo("#ffc9ba")
-//                 hasPlugin("project.one") -> DependencyInfo("#fcb96a")
-//                 else -> null
-//             }
-//         }
-//     }
-// }
+dependencyGraphConfig {
+    projectName = "dependency-graph-sample"
+    outputFormat = OutputFormat.SVG
+
+    dependencyBuilder { project ->
+        with(project.plugins) {
+            when {
+                hasPlugin("project.three") -> DependencyInfo("#81d4fa", isBoxShape = true)
+                hasPlugin("project.two") -> DependencyInfo("#ffc9ba")
+                hasPlugin("project.one") -> DependencyInfo("#fcb96a")
+                else -> null
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
